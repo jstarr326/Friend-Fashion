6f38907439bac6eed1ea0534e307503ab1f73e97
jstarr326 <jstarrtaylor@gmail.com>
2016-01-28 19:29:01 -0800

    changes in class

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-  config.omniauth :facebook, "1259845757362855", "4374cde3a0e74941d767bc02efc7beff"
+
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,6 @@
+OmniAuth.config.logger = Rails.logger
+Rails.application.config.middleware.use OmniAuth::Builder do
+ provider :facebook, '1259845757362855', "4374cde3a0e74941d767bc02efc7beff",
+ {:scope => 'user_friends, email, public_profile' , :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}}
+end
+
