8acab7fae1e730e37955f6c1d309b4bc4244f91f
jstarr326 <jstarrtaylor@gmail.com>
2016-01-30 14:06:14 -0800

    revert

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,6 @@ def self.new_with_session(params, session)
      super.tap do |user|
        if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
          user.email = data["email"] if user.email.blank?
-         user.image = access_token.info.image
        end
      end
  end
