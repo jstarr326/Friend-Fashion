5e02c648ea2a66d9596c2ed7291654833c063194
jstarr326 <jstarrtaylor@gmail.com>
2016-02-01 19:27:14 -0800

    adding some forms

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,14 +4,32 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @items = item.new()
   end
 
   def create
+    @item = Item.new(safe_item_params)
+    if @item.save()
+      redirect_to @item
+    else
+      render 'new'
+    end
   end
+
+    def update
+    if @item.update(safe_item_params)
+        #if the shirt was successfully saved, redirect to the show shirt page
+      redirect_to @item
+    else
+        #if the shirt wasn't successfully saved, render the "new.html" view (allow user to edit it and try saving again)
+      render 'edit'
+    end
+    end
 
   def show
   end
 
-  def safe_items_params
+  def safe_item_params
+    params.require('item').permit(:name, :description, :category, :size)
   end
 end
